cfd39ef46f294d4d9ca0221b17414fc6f5d0fe40
Brian Greene <bgreene623@gmail.com>
2016-03-21 14:36:09 -0400

    Starting project with new clean app.js file

---
+++ js/app.js
@@ -0,0 +1,14 @@
+(function () {
+  'use strict';
+
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+
+  initMap();
+
+}());
